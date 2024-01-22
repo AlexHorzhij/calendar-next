d6c497b6093600fdaef62785945905b82c807c6d
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-01-22 21:07:01 +0200

    update cnfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: {
+    method: "static",
+    path: "./out",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

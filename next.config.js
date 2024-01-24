9c6091bcb6b2b3a0c248ccc4f3908958f2f68130
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-01-24 16:34:37 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
+const nextConfig = {};
 
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {};
-  }
-
-  return {
-    output: "export",
-  };
-};
+module.exports = nextConfig;

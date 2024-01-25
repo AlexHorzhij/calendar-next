41a6e9b41ad6214d9666cc60db9831e74e970e0d
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-01-25 10:15:25 +0200

    v

--- next.config.js
+++ next.config.js
@@ -2,17 +2,19 @@
  * @type {import('next').NextConfig}
  */
 
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+// const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {};
-  }
-  const nextConfig = {
-    basePath: "/calendar-next",
-    assetPrefix: "/calendar-next",
-    output: "export",
-  };
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     return {};
+//   }
+//   const nextConfig = {
+//     basePath: "/calendar-next",
+//     assetPrefix: "/calendar-next",
+//     output: "export",
+//   };
 
-  return nextConfig;
-};
+//   return nextConfig;
+// };
+
+module.exports = {};
